cccd3ea933c6942062e26795300736d5c968db31
Bruno Caceres <brunocaceres@live.com>
2024-07-19 07:25:57 -0300

    pdf

--- src/pages/TelasOrdensFab/OFVirtual/index.js
+++ src/pages/TelasOrdensFab/OFVirtual/index.js
@@ -1,23 +1,107 @@
+import React, {useState} from 'react';
+import {StyleSheet, Dimensions, Text, View} from 'react-native';
+import Pdf from 'react-native-pdf';
+import {Slider} from '@react-native-assets/slider';
+import _ from 'lodash';
 
-import React from 'react';
-import {
-    StyleSheet,
-    View
-} from 'react-native';
+export default function OFVirtual() {
+  const source = require('../../../assets/thereactnativebook-sample.pdf');
 
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(null);
+  const pdfRef = React.useRef();
 
-const OFVirtual = () => {
+  const throttleOnSliderValueChange = React.useCallback(
+    _.throttle(value => {
+      pdfRef.current && pdfRef.current.setPage(Math.floor(value));
+    }, 200),
+    [],
+  );
+
+  const onSlidingComplete = value => {
+    pdfRef.current && pdfRef.current.setPage(Math.floor(value));
+  };
+
+  const CustomThumb = ({value}) => {
     return (
-        <View style={styles.container}>
+      <View style={styles.thumbContainer}>
+        <View style={styles.thumbTextContainer}>
+          <Text style={styles.thumbText}>{Math.floor(value)}</Text>
         </View>
-    )
+      </View>
+    );
+  };
+  return (
+    <View style={styles.container}>
+      <Pdf
+        trustAllCerts={false}
+        source={source}
+        onLoadComplete={(numberOfPages, filePath) => {
+          setTotalPages(numberOfPages);
+        }}
+        onPageChanged={(page, numberOfPages) => {
+          setCurrentPage(page);
+        }}
+        onError={error => {
+          console.log(error);
+        }}
+        onPressLink={uri => {
+          console.log(`Link pressed: ${uri}`);
+        }}
+        style={styles.pdf}
+      />
+      <View style={styles.verticalSliderContainer}>
+        <Slider
+          value={currentPage}
+          minimumValue={1}
+          maximumValue={totalPages}
+          step={0}
+          minimumTrackTintColor="white"
+          maximumTrackTintColor="white"
+          vertical={true}
+          slideOnTap={true}
+          onValueChange={throttleOnSliderValueChange}
+          onSlidingComplete={onSlidingComplete}
+          CustomThumb={CustomThumb}
+        />
+      </View>
+    </View>
+  );
 }
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        padding: 20,
-        backgroundColor: '#fff',
-    },
-})
 
-export default OFVirtual
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 55,
+  },
+
+  pdf: {
+    flex: 1,
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+  verticalSliderContainer: {
+    justifyContent: 'center',
+    alignItems: 'flex-end',
+    marginBottom: 30,
+    marginRight: 0,
+  },
+  thumbContainer: {
+    backgroundColor: 'darkcyan',
+    padding: 20,
+    borderRadius: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  thumbTextContainer: {
+    width: 20,
+    height: 20,
+  },
+  thumbText: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+});
--- src/pages/TelasOrdensFab/index.js
+++ src/pages/TelasOrdensFab/index.js
@@ -1,69 +1,72 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import {
-    StyleSheet
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
 } from 'react-native';
-import { RNCamera } from 'react-native-camera';
+//import { RNCamera } from 'react-native-camera';
 import api from '../../services/api.js';
-const TelasOrdensFab = ({ navigation }) => {
-    const [barcode, setBarcode] = useState('118659.00');
+const TelasOrdensFab = ({navigation}) => {
+  const [barcode, setBarcode] = useState('118659.00');
 
-    const fnPesquisar = async () => {
-        try {
-            const apiInstance = await api();
-            const response = await apiInstance.get(
-                `/ordens?evento=20&numeroOF=${barcode}`,
-            );
-            console.log(response.data)
-        } catch (error) {
-            console.error('Erro ao obter dados da API:', error);
-            setBarcode('');
-        }
-    };
-    const handleBarcodeDetected = ({ barcodes }) => {
-        if (barcodes.length > 0) {
-            const scannedData = barcodes[0].data;
-            console.log('Scanned data:', scannedData);
-            // Aqui você pode fazer o que quiser com os dados lidos
-        }
-    };
+  const fnPesquisar = async () => {
+    try {
+      const apiInstance = await api();
+      const response = await apiInstance.get(
+        `/ordens?evento=20&numeroOF=${barcode}`,
+      );
+      console.log(response.data);
+    } catch (error) {
+      console.error('Erro ao obter dados da API:', error);
+      setBarcode('');
+    }
+  };
+  const teste = async () => {
+    navigation.navigate('Tab');
+  };
 
-    return (
-        <View style={{ flex: 1 }}>
-            <RNCamera
-                style={{ flex: 1 }}
-                onBarCodeRead={handleBarcodeDetected}
-            />
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        placeholder="Digite algo"
+        value="118659.00"
+      />
+      <TouchableOpacity style={styles.button} onPress={teste}>
+        <Text style={styles.buttonText}>OK</Text>
+      </TouchableOpacity>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
-
-    },
-    input: {
-        width: 200,
-        height: 40,
-        borderWidth: 1,
-        borderColor: '#ccc',
-        borderRadius: 5,
-        paddingHorizontal: 10,
-        marginRight: 10,
-        color: "#000"
-    },
-    button: {
-        backgroundColor: '#09A08D',
-        paddingVertical: 10,
-        paddingHorizontal: 20,
-        borderRadius: 5,
-    },
-    buttonText: {
-        color: '#fff',
-    },
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  input: {
+    width: 200,
+    height: 40,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    marginRight: 10,
+    color: '#000',
+  },
+  button: {
+    backgroundColor: '#09A08D',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: '#fff',
+  },
 });
 
 export default TelasOrdensFab;
